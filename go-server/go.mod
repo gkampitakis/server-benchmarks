922fac22560e58962728a4733a4e800a912af36f
gkampitakis <gkabitakis@gmail.com>
2020-09-19 21:03:41 +0100

    add basic health check route

--- go-server/go.mod
+++ go-server/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gkampitakis/go-webserver v0.0.0-20200913161337-52019aea5849
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mackerelio/go-osstat v0.1.0
 )
--- go-server/server.go
+++ go-server/server.go
@@ -4,13 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"github.com/mackerelio/go-osstat/cpu"
+	"github.com/mackerelio/go-osstat/loadavg"
+	"github.com/mackerelio/go-osstat/memory"
+	"github.com/mackerelio/go-osstat/network"
+	"github.com/mackerelio/go-osstat/uptime"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type Response struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+}
+
+type ServerStats struct {
+	CPU     *cpu.Stats      `json:"cpu"`
+	Memory  *memory.Stats   `json:"memory"`
+	Loadavg *loadavg.Stats  `json:"loadavg"`
+	Uptime  time.Duration   `json:"uptime"`
+	Network []network.Stats `json:"network"`
 }
 
 type BodyJSON interface{}
@@ -22,6 +37,18 @@ func ListenCtrl(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Println(JSONStringify(query))
 
 	_, err := w.Write(newJSONResponse("Responding back", query))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func HealthCtrl(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	stats := getStats()
+
+	_, err := w.Write(newJSONResponse("Health Status", stats))
 
 	if err != nil {
 		panic(err)
@@ -56,6 +83,7 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/listen", ListenCtrl)
+	router.GET("/health", HealthCtrl)
 	router.POST("/postdata", PostDataCtrl)
 
 	log.Printf("Go Server listening on port %v 🚀", 5000)
@@ -99,4 +127,24 @@ func newJSONResponse(message string, data interface{}) (res []byte) {
 	}
 
 	return
+}
+
+func getStats() *ServerStats {
+	c, err := cpu.Get()
+	m, err := memory.Get()
+	n, err := network.Get()
+	u, err := uptime.Get()
+	l, err := loadavg.Get()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &ServerStats{
+		Memory:  m,
+		CPU:     c,
+		Network: n,
+		Uptime:  u,
+		Loadavg: l,
+	}
 }
